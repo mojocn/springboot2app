213a308be07a6fc08ab4ea343fa5360d82c85edb
EricZhou <zhouqing1@360.cn>
2019-11-01 13:55:24 +0800

    readme

--- src/main/java/springboot2app/Application.java
+++ src/main/java/springboot2app/Application.java
@@ -1,30 +1,30 @@
 package springboot2app;
 
-import springboot2app.model.RoleEntity;
-import springboot2app.service.RoleService;
-import springboot2app.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springboot2app.model.RoleEntity;
+import springboot2app.service.RoleService;
+import springboot2app.service.UserService;
 
 @SpringBootApplication
 public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("======================");
-        System.out.println("webapi service start...");
+        System.out.println("service start at :9527");
         System.out.println("======================");
     }
 
     @Bean
     public CommandLineRunner firstRows(UserService userService, RoleService roleService) {
         return (args) -> {
+            // 创建初始化数据
             RoleEntity role = roleService.createInitRow();
             userService.createGodUser(role);
         };
